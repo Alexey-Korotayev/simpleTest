64bb8c22818ef386103d98689f60e875388e774c
Alexey_Korotayev <Alexey_Korotayev@epam.com>
2019-02-04 10:08:07 +0600

    one project with profile

---
+++ src/main/java/com/epam/App.java
@@ -0,0 +1,22 @@
+package com.epam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println(saySomething());
+    }
+
+    /**
+     * @return
+     */
+    public static String saySomething() {
+        return "Hello EPAM !";
+    }
+}
